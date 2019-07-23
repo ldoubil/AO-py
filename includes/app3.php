32771632939e7cb66bb16681f3698089a6fed0a2
ldoubil <baikaiwen12@outlook.com>
2019-07-23 18:17:11 +0800

    4.6.6

--- includes/app3.php
+++ includes/app3.php
@@ -1,50 +1,39 @@
 <script>
 
-
-    //判断是否是手机
-    function IsMobile() {
-        var isMobile = {
-            Android: function () {
-                return navigator.userAgent.match(/Android/i) ? true : false;
-            },
-            BlackBerry: function () {
-                return navigator.userAgent.match(/BlackBerry/i) ? true : false;
-            },
-            iOS: function () {
-                return navigator.userAgent.match(/iPhone|iPad|iPod/i) ? true : false;
-            },
-            Windows: function () {
-                return navigator.userAgent.match(/IEMobile/i) ? true : false;
-            },
-            any: function () {
-                return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Windows());
-            }
-        };
-
-        return isMobile.any(); //是移动设备
-    }
-    $(function () {
-        // undefined
-        function APP3() {
-            var ifundefined = $("#inn-widget__author-profile__count").find(".inn-widget__author-profile__count__item__number").first().text();
-            if (ifundefined != "undefined") {
-                if (IsMobile()) {
-                    $(".inn-widget__author-profile__container").children(".inn-widget__author-profile__avatar").remove();
-                    $("#inn-widget__author-profile__info").remove();
-                    $("#inn-widget__author-profile__point").remove();
-                    $(".inn-widget__author-profile__container").attr("class", "inn-widget__author");
-                    $(".inn-widget,.inn-sidebar__widget,.inn-widget__author-profile").attr("class", "inn-widget inn-sidebar__widget inn-widget__author");
-                    var avatarhtml = $(".poi-g_lg-1-4").children(".inn-sidebar__item").prop("outerHTML");
-                    $(".poi-g_lg-1-4").children(".inn-sidebar__item").remove();
-                    $("#inn-comment").before(avatarhtml);
+        $(function () {
+            function IsPhone() {
+                mobile_flag = false;
+                var screen_width = window.screen.width;
+                var screen_height = window.screen.height;
+                if (screen_width < 500 && screen_height < 820) {
+                    mobile_flag = true;
                 }
+                return mobile_flag;
             }
-            clearInterval(xh2);
-        }
+        
+            function APP3() {
+                console.log(IsPhone());
+                
+                var ifundefined = $("#inn-widget__author-profile__count").find(".inn-widget__author-profile__count__item__number").first().text();
+                if (ifundefined != "undefined") {
+                    clearInterval(xh2);
+                    if (IsPhone()) {
+                        $(".inn-widget__author-profile__container").children(".inn-widget__author-profile__avatar").remove();
+                        $("#inn-widget__author-profile__info").remove();
+                        $("#inn-widget__author-profile__point").remove();
+                        $(".inn-widget__author-profile__container").attr("class", "inn-widget__author");
+                        $(".inn-widget,.inn-sidebar__widget,.inn-widget__author-profile").attr("class", "inn-widget inn-sidebar__widget inn-widget__author");
 
-
-        var xh2 = setInterval(APP3, 500);
-    })
-
-
-</script>
+                        // inn-comment
+                    }
+                }
+                
+            }
+        
+            
+                // undefined
+                var xh2 = setInterval(APP3, 500);
+            })
+        
+        
+        </script>
