c96e2992e8dd0184739ff518aa7206244898b6cb
ldoubil <baikaiwen12@outlook.com>
2019-07-23 15:33:13 +0800

    4.5.8

--- AO-PY.php
+++ AO-PY.php
@@ -3,7 +3,7 @@
 Plugin Name: AO-PY
 Plugin URI:	https://github.com/ldoubil/AO-py
 Description:  爱国Ao第三方扩展！。
-Version: 4.5.7
+Version: 4.5.8
 Author: 会做饭的二哈
 Author URI: mailto:baikaiwen12@outlook.com
 License: A "Slug" license name e.g. GPL2
@@ -80,9 +80,11 @@ function addjq(){                   //引入jq代码
 //--------------------------------------------------------------------------
 function addapp1(){
     // includes_url($path, $scheme)
-    if (is_home()) {
+    if (is_single()) {
+        
+    }else(
         include(plugin_dir_path(__FILE__ ).'/includes/app1.php');
-    }
+    )
     // 只有首页才进行修改操作
     // echo('<script src="'."></script>');
 
