d7bbac9a546e464427b22438353eb190b30d69fd
ldoubil <baikaiwen12@outlook.com>
2019-07-24 10:37:43 +0800

    不再开源

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$AO_config = array(
-    'menu_title'      =>  'AO-PY配置'      ,
-    'menu_capability' =>  'manage_options' ,
-    'menu_slug'       =>  'ao-config'      ,
-    'menu_function'   =>  'menutext'
-
- );
